109181e498af3c11c98fe66bce6aeba53b8b7e94
Bedprkash Rout <bedprakash@verse.in>
2017-01-18 19:02:45 +0530

    added logicc for keyword based verification in OTP matching logic

--- app/src/main/java/git/bpr10/otpreader/utils/OtpMatcher.java
+++ app/src/main/java/git/bpr10/otpreader/utils/OtpMatcher.java
@@ -1,7 +1,9 @@
 package git.bpr10.otpreader.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,18 +17,30 @@ public class OtpMatcher {
 
   private static String OTP_PATTERN = "\\b(?=\\w)[0-9]{4,8}\\b(?<=\\w)";
 
+  private static List<String> keywords = Arrays.asList("OTP", "One time password", "code", "key");
+
   /**
    * @param msg - Sms text
    * @return OTP string if found else null
    */
   public static String checkForOTP(String msg) {
     ArrayList<OTP> otps = getMatchingCandidates(msg);
-    if (!otps.isEmpty()) {
+    if (!otps.isEmpty() || msgContainsKeywords(msg)) {
       Collections.sort(otps, Collections.reverseOrder());
       System.out.print(otps.get(0).otp);
       return otps.get(0).otp;
     }
     return null;
+  }
+
+  private static boolean msgContainsKeywords(String msg) {
+    for (String key : keywords) {
+      if (msg.contains(key) || msg.contains(key.toLowerCase()) ||
+          msg.contains(key.toUpperCase())) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private static ArrayList<OTP> getMatchingCandidates(String text) {
